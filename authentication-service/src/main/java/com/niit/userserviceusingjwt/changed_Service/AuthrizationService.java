17132633171b90ca1e8f21baa59913436b324aac
Sidharth Mishra <mishrasidharth83@gmail.com>
2023-04-25 19:44:06 +0530

    new Changes

--- authentication-service/src/main/java/com/niit/userserviceusingjwt/changed_Service/AuthrizationService.java
+++ authentication-service/src/main/java/com/niit/userserviceusingjwt/changed_Service/AuthrizationService.java
@@ -4,7 +4,9 @@ import com.niit.userserviceusingjwt.model.User;
 import com.niit.userserviceusingjwt.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
 
+@Service
 public class AuthrizationService {
     @Autowired
     private JwtService jwtService;
--- authentication-service/src/main/java/com/niit/userserviceusingjwt/changed_Service/JwtService.java
+++ authentication-service/src/main/java/com/niit/userserviceusingjwt/changed_Service/JwtService.java
@@ -14,7 +14,7 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Function;
-
+@Component
 public class JwtService {
 
     public static final String SECRET = "5367566B59703373367639792F423F4528482B4D6251655468576D5A71347437";
